25164de0b48eb0d4373e230f2325194d8c1892da
sanjaySDET <sanjaybabusdet@gmail.com>
2022-12-14 21:27:44 +0530

    1st commit

---
+++ sanjay/src/test/java/sample/Test1.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+@Test
+public void a1()
+{
+	System.out.println("hi");
+}
+}
---
+++ sanjay/src/test/java/sample/Test2.java
@@ -0,0 +1,11 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void b1()
+{
+	System.out.println("hello");
+}
+}
